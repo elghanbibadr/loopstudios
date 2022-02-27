0ba0e4678264791a13ac8072ab03666484b74fce
badr <bghanbi50@gmail.com>
2022-02-27 13:50:08 +0100

    nav

--- loopstudios-landing-page-main/app/index.js
+++ loopstudios-landing-page-main/app/index.js
@@ -1,3 +1,12 @@
-// const hamburger=document.querySelector('.nav__hamburger');
+ const hamburger=document.querySelector('.nav__hamburger');
 // const close=document.querySelector('.nav__close');
-const container=document.querySelector('.nav-container');
+// const container=document.querySelector('.nav-container');
+//
+const list=document.querySelector('.nav__list');
+container.addEventListener('click',function(){
+     container.classList.toggle('collapse');
+     list.classList.toggle('collapse');
+ })
+// hamburger.addEventListener('click',function(){
+//     list.classList.toggle('collapse');
+// })
