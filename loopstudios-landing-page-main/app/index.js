850272f3ec9d96f6deea1c82cd14b76cdf860d3b
badr <bghanbi50@gmail.com>
2022-03-01 15:51:02 +0100

    fixing nav issue

--- loopstudios-landing-page-main/app/index.js
+++ loopstudios-landing-page-main/app/index.js
@@ -1,12 +1,19 @@
- const hamburger=document.querySelector('.nav__hamburger');
-// const close=document.querySelector('.nav__close');
-// const container=document.querySelector('.nav-container');
-//
+const hamburger=document.querySelector('.nav__hamburger');
+const closee=document.querySelector('.nav__close');
 const list=document.querySelector('.nav__list');
-container.addEventListener('click',function(){
-     container.classList.toggle('collapse');
+const head=document.querySelector('.header');
+ hamburger.addEventListener('click',function(){
+     hamburger.classList.toggle('collapse');
+     closee.classList.toggle('collapse');
      list.classList.toggle('collapse');
+     head.classList.toggle('collapse');
+
  })
-// hamburger.addEventListener('click',function(){
-//     list.classList.toggle('collapse');
-// })
+ closee.addEventListener('click',function(){
+     hamburger.classList.toggle('collapse');
+     closee.classList.toggle('collapse');
+     list.classList.toggle('collapse');
+     head.classList.toggle('collapse');
+
+
+ })
